7edef2795cc586daacd78815832c1f62a0cd37a1
Godwin Agbudumeh <godwinagbudumeh@gmail.com>
2024-10-22 13:41:46 +0100

    used is user authenticated in dashboard true or false

--- client/src/layouts/dashboardLayout/DashboardLayout.jsx
+++ client/src/layouts/dashboardLayout/DashboardLayout.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Outlet, useNavigation } from 'react-router-dom';
 import './dashboardLayout.css'
-import {useAuth} from '@clerk/clerk-React';
+import {useAuth} from '@clerk/clerk-react';
 
 export default function DashboardLayout() {
-const {userId} = useAuth()
+  const {userId, isLoaded} = useAuth();
+
+  const navigate = useNavigation();
+
+  useEffect(()=>{
+    if(isLoaded && !userId){
+      navigate("/sign-in ")
+    }
+  }, [])
+
 
   return (
     <div className='dashboardLayout'>
