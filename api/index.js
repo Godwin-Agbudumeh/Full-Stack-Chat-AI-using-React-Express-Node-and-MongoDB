81ffdfa28cfb2c734bd86b653cde0e841bc1973e
Godwin Agbudumeh <godwinagbudumeh@gmail.com>
2024-10-30 16:56:18 +0100

    changed mongodb connect

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000;
 const app = express();
 const connect = async ()=>{
     try{
-        await mongoose.connect(process.env.MONGO);
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log("connected to mongodb")
     }catch(err){
         console.log(err)
